49b5a55a38f1cc64eb612e9ab5fb67574ac65b18
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-20 07:49:22 +0300

    Add classes for article and source objects.

---
+++ app/models.py
@@ -0,0 +1,27 @@
+class Article:
+    '''
+    This class defines News Articles Objects.
+    '''
+
+    def __init__(self, author, title, description, url, urlToImage, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+
+
+class Source:
+    '''
+     This class defines News Source Objects.
+    '''
+
+    def __init__(self, id, name, description, url, category, language, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
