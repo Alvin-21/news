2de093c0c324f5daa4ca1eb71dc16538d3745c07
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-20 09:01:12 +0300

    Add method to test if article instance is created.

--- tests/test_article.py
+++ tests/test_article.py
@@ -13,6 +13,11 @@ class ArticleTest(unittest.TestCase):
 
         self.new_article = Article("David Murphy", "What You Need to Know About Buying Cryptocurrency on PayPal", "Whether you’re looking to make a larger investment or you just want to dabble in cryptocurrencies, you can purchase Bitcoin, Ethereum, Bitcoin Cash, and Litecoin through PayPal. And, soon, you’ll be able to pay various merchants using your cryptocurrency stas…",  "https://lifehacker.com/what-you-need-to-know-about-buying-cryptocurrency-on-pa-1846585705",  "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/qvc2foo4ufow1cbsuk9f.jpg", "2021-03-31T14:00:00Z")
 
-   
+    def test_instance(self):
+        '''
+        Test case to test if object instance is created.
+        '''
+
+        self.assertTrue(isinstance(self.new_article, Article))
 
     
