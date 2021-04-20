bf2025810fac19449ad4246a412d304dd28dc935
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-20 09:38:59 +0300

    Add method to test if object is initialised properly.

--- tests/test_source.py
+++ tests/test_source.py
@@ -19,3 +19,16 @@ class SourceTest(unittest.TestCase):
         '''
 
         self.assertTrue(isinstance(self.new_source, Source))
+
+    def test_init(self):
+        '''
+        Test case to test if object is initialised properly.
+        '''
+
+        self.assertEqual(self.new_source.id, "abc-news")
+        self.assertEqual(self.new_source.name, "ABC News")
+        self.assertEqual(self.new_source.description, "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.")
+        self.assertEqual(self.new_source.url, "https://abcnews.go.com")
+        self.assertEqual(self.new_source.category, "general")
+        self.assertEqual(self.new_source.language, "en")
+        self.assertEqual(self.new_source.country, "us")
