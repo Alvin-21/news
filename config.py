9ff062f77ebdd9ffa0e3b0a081158b479fcd4c5a
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-20 07:38:54 +0300

    Fix typo.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    EVERYTHING_BASE_URL = "https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}"
+    ARTICLE_BASE_URL = "https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}"
     SOURCE_BASE_URL = "https://newsapi.org/v2/sources?language=en&category={}&apiKey={}"
     TOP_HEADLINES_URL = "https://newsapi.org/v2/top-headlines?language=en&apiKey={}"
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
