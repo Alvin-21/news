248b286860f26f7de2278392e1757c9e71a85eaf
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-21 09:48:57 +0300

    Add index page view function.

---
+++ app/main/views.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, redirect, url_for
+from . import main
+from ..requests import get_articles, get_source, get_topheadlines
+
+# Views
+@main.route('/')
+def index():
+    '''
+    Function that returns the index page and its data.
+    '''
+
+    business_news = get_source("business")
+    entertainment_news = get_source("entertainment")
+    general_news = get_source("general")
+    health_news = get_source("health")
+    science_news = get_source("science")
+    sports_news = get_source("sports")
+    technology_news = get_source("technology")
+
+    title = "Home | News Sources"
+
+    return render_template('index.html', title=title, business=business_news, entertainment=entertainment_news, general=general_news, health=health_news, science=science_news, sports=sports_news, technology=technology_news)
