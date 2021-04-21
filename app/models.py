30c550b5a6cdc22f267c28c01114a5347f78872f
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-21 18:32:48 +0300

    Add navbar block and fix bug.

--- app/models.py
+++ app/models.py
@@ -17,11 +17,10 @@ class Source:
      This class defines News Source Objects.
     '''
 
-    def __init__(self, id, name, description, url, category, language, country):
+    def __init__(self, id, name, description, url, category, language):
         self.id = id
         self.name = name
         self.description = description
         self.url = url
         self.category = category
         self.language = language
-        self.country = country
